be521bd55501bebded55afc922db5ee3ec118f9e
Shadow4546 <89007456+Shadow4546@users.noreply.github.com>
2024-05-29 14:43:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("AUTH_USERS")
+    BOT_TOKEN = os.environ.get("7012366714:AAG5gy8YWMlK3jmoMTkcJi-IHJD8LM0w1v0")
+    API_ID = int(os.environ.get("21919660"))
+    API_HASH = os.environ.get("ce3325b4fd5342682f74160cf2ff5484")
+    AUTH_USERS = os.environ.get("6598369009")
